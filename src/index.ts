f52e1532060311ecce8ea36d4999645f932e9cd3
marsnebulasoup <marsnebulasoup@gmail.com>
2022-08-13 21:40:22 -0500

    Added rough demo script

---
+++ src/index.ts
@@ -0,0 +1,70 @@
+import { HoneyPot } from './honeypot/honeypot';
+import { TLInit } from './login';
+import * as fs from "fs";
+import { InputPeerUserArray } from './tl/input-peer-user-array';
+import { Api } from 'telegram';
+
+const ENV_FILE_PATH = ".tl.env"
+const AI_FOLDER_NAME = "FOLDER_TEST";
+
+const saveCredentials = (apiId, apiHash, session) => {
+  fs.writeFileSync(ENV_FILE_PATH, [
+    `API_ID=${apiId}`,
+    `API_HASH=${apiHash}`,
+    `SESSION=${session}`
+  ].join("\n"), {
+
+  })
+}
+(async () => {
+  const client = await TLInit.init(ENV_FILE_PATH);
+  let honeypots: HoneyPot[] = [];
+
+  let scammerFolderId = await client.getFolderIdByName(AI_FOLDER_NAME);
+  let scammerUserIds: InputPeerUserArray = await client.getPeersInFolder(scammerFolderId);
+
+  const refreshHoneyPots = async () => {
+    // Remove honeypots not in the scammer folder
+
+    console.log(scammerUserIds);
+
+    honeypots = honeypots.filter((honeypot, index) => {
+      const isInFolder = scammerUserIds.includes(honeypot.chatId);
+      console.log(`${honeypot.chatId.userId} is in folder: ${isInFolder}`);
+      if(!isInFolder) {
+        honeypot.destroy();
+      }
+      return isInFolder;
+    });
+
+    console.log(honeypots);
+
+    // Add any new honeypots
+    scammerUserIds.forEach(async (user) => {
+      if (!honeypots.find(honeypot => honeypot.chatId.userId.toString() == user.userId.toString())) {
+        honeypots.push(
+          await HoneyPot.create({
+            tlClient: client,
+            chatId: user,
+          })
+        );
+      }
+    });
+  }
+
+  refreshHoneyPots();
+
+  // When chat is added or removed to a folder
+  client.subscribe({
+    eventType: "UpdateDialogFilter",
+    callback: async (_: Api.UpdateDialogFilter) => {
+      // console.log(JSON.stringify(_, null, 2));
+      scammerFolderId = await client.getFolderIdByName(AI_FOLDER_NAME); // search for folder id in case it was deleted or hasn't been created yet
+      if (scammerFolderId) {
+        scammerUserIds = await client.getPeersInFolder(scammerFolderId);
+        await refreshHoneyPots();
+        // console.log(`AI folder was updated -> ${JSON.stringify(scammerUserIds, null, 2)}`);
+      }
+    }
+  });
+})();
---
+++ src/login.ts
@@ -0,0 +1,45 @@
+import { TLClient } from './tl/client';
+import { StringSession } from "telegram/sessions";
+import input from "input";
+import { config } from "dotenv";
+import * as fs from "fs";
+
+export class TLInit {
+  static saveCredentials(apiId, apiHash, session, path) {
+    fs.writeFileSync(path, [
+      `API_ID=${apiId}`,
+      `API_HASH=${apiHash}`,
+      `SESSION=${session}`
+    ].join("\n"), {
+
+    })
+  }
+
+  static async init(path: string) {
+    let apiHash, apiId, session;
+    const configuration = config({ path: path });
+    if (configuration.parsed && configuration.parsed["API_ID"] && configuration.parsed["API_HASH"]) {
+      const { API_ID, API_HASH, SESSION } = configuration.parsed
+      console.log("Using saved credentials...");
+      console.log(" > API_ID", API_ID);
+      console.log(" > API_HASH", API_HASH);
+      apiId = Number(API_ID);
+      apiHash = String(API_HASH);
+      session = new StringSession(SESSION || "");
+    }
+    else {
+      apiId = await input.text("Enter API Id: ");
+      apiHash = await input.text("Enter API hash: ");
+      session = new StringSession("");
+    }
+    const client = new TLClient({ apiId, apiHash, session });
+    await client.init({
+      getPhoneNumberIfNeeded: async () => await input.text("Enter phone number: "),
+      getPasswordIfNeeded: async () => await input.text("Enter password: "),
+      getPhoneCodeIfNeeded: async () => await input.text("Enter phone code: "),
+    });
+    this.saveCredentials(apiId, apiHash, client.session.save(), path);
+    console.log("Login successful!");
+    return client;
+  }
+}

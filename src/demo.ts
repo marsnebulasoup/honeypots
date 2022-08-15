3b3532fc5c4c477a6d9b9eff4d36d12b5a5e7f3d
marsnebulasoup <marsnebulasoup@gmail.com>
2022-08-14 21:40:03 -0500

    added user instructions

--- src/demo.ts
+++ src/demo.ts
@@ -6,6 +6,7 @@ const AI_FOLDER_NAME = "HONEYPOT";
 
 (async () => {
   const client = await TLInit.init(ENV_FILE_PATH);
+  console.info(`ℹ️ Create a folder in Telegram called ${AI_FOLDER_NAME}, move scammer chats in, and the AI will reply to them.`);
   const honeypots = new HoneyPots({ 
     client: client, 
     honeypotFolderName: AI_FOLDER_NAME 
